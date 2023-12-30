c3ed11c00db22a1110e50b17d40802bfaafaacd2
MilkyWay <112637061+mkway999@users.noreply.github.com>
2023-12-30 16:49:26 +0900

    Add files via upload

--- 04.자판기/vending.js
+++ 04.자판기/vending.js
@@ -9,3 +9,25 @@ function inputMoney()
     document.getElementById("txt_input").value="";
     document.getElementById("txt_input").focus();
 }
+
+function subCount(objId)
+{
+    var product = document.getElementById(objId);
+    if(Number(product.value)<=0)
+    {
+        alert("최수 주문 수량은 1개 이상입니다.");
+        return;
+    }
+    product.value = Number(product.value)-1;
+}
+
+function addCount(objId)
+{
+    var product = document.getElementById(objId);
+    if(Number(product.value)>=99)
+    {
+        alert("최대 주문 수량은 99개 입니다.");
+        return;
+    }
+    product.value = Number(product.value)+1;
+}
